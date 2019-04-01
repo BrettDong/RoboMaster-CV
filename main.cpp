ba14050d687458e77741d1fba7a0b1e38be2ff70
Brett Dong <brett.browning.dong@gmail.com>
2019-04-02 01:45:11 +0800

    Test environment for control surface

--- main.cpp
+++ main.cpp
@@ -95,26 +95,23 @@ int main(int argc, char *argv[])
     signal(SIGKILL, sig_handler);
     cout << "INPUT:  CAMERA " << img.cols << "x" << img.rows << endl;
     cout << "OUTPUT: " << serial_device << endl;
+    float yaw = 0.0f, pitch = 0.0f;
+    bool send = true;
     while(running)
     {
         auto Tstart = chrono::system_clock::now();
         if(!cap.read(img))
             break;
-        if(constraint_set::DetectArmor(img, target))
+        constraint_set::DetectArmor(img, target);
+        if(send)
         {
-            float yaw = atan2(target.x, target.z), pitch = atan2(target.y, sqrt(target.x*target.x + target.z*target.z));
-            yaw = yaw / M_PI * 180; pitch = pitch / M_PI * 180;
+            write(img, "[TX]", cv::Point(10, 40));
             protocol::Send(yaw * angle_amp, pitch * angle_amp);
-            if(verbose > 0)
-            {
-                sprintf(buf, "Detected @(% 6.2f, % 6.2f, % 6.2f)", target.x, target.y, target.z);
-                write(img, buf, cv::Point(10, 40));
-                sprintf(buf, "YAW=% 4.2fDEG PITCH=% 4.2fDEG", yaw, pitch);
-                write(img, buf, cv::Point(10, 60));
-                sprintf(buf, "AMP%fx YAW= % 4.2fDEG PITCH=% 4.2fDEG", angle_amp, yaw * angle_amp, pitch * angle_amp);
-                write(img, buf, cv::Point(10, 80));
-            }
         }
+        sprintf(buf, "YAW=% 4.2fDEG PITCH=% 4.2fDEG", yaw, pitch);
+        write(img, buf, cv::Point(10, 60));
+        sprintf(buf, "AMP%fx YAW= % 4.2fDEG PITCH=% 4.2fDEG", angle_amp, yaw * angle_amp, pitch * angle_amp);
+        write(img, buf, cv::Point(10, 80));
         if(verbose > 0)
         {
             auto Tend = chrono::system_clock::now();
@@ -131,12 +128,19 @@ int main(int argc, char *argv[])
                     running = false;
                     break;
                 case 'a':
-                    if(angle_amp > 0.5f)
-                        angle_amp -= 0.5f;
+                    yaw += 1.0f;
                     break;
                 case 'd':
-                    if(angle_amp < 9.5f)
-                        angle_amp += 0.5f;
+                    yaw -= 1.0f;
+                    break;
+                case 'w':
+                    pitch += 1.0f;
+                    break;
+                case 's':
+                    pitch -= 1.0f;
+                    break;
+                case 'f':
+                    send = !send;
                     break;
             }
         }
